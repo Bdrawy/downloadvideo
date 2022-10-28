1d02de240d027604b2e72eb92a00ac82c9234653
Bdrawy <116612531+Bdrawy@users.noreply.github.com>
2022-10-28 21:40:05 +0200

    Update vddown.js

--- vddown.js
+++ vddown.js
@@ -5,5 +5,5 @@
     downloadVideo(link,fromate);
   });
   function downloadVideo(link,fromate) {
-      $('.download-video').html('<iframe style="width:100%;height:60px;border:0;overflow:hidden;" scrolling="no" src="https://loader.to/api/card/?url='+link+'&f='+fromate+'adUrl=https://www.downyoutube.tk/"></iframe>');
+      $('.download-video').html('<iframe style="width:100%;height:60px;border:0;overflow:hidden;" scrolling="no" src="https://loader.to/api/button/?url='+link+'&f='+fromate+'&color=0072ec&adUrl=https://www.downyoutube.tk/"></iframe>');
   }
